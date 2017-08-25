d308d8a04d9dd1f1424938e0d6316451a9e93047
alastair38 <alastair@alastaircox.com>
2017-08-25 15:13:18 +0100

    Version 1.32 - slight changes to author header layout. Inc phone number

--- author.php
+++ author.php
@@ -4,6 +4,7 @@ $author = get_queried_object();
 $author_id = $author->ID;
 $user_image = get_field('user_image', 'user_' . $author_id  . '');
 $work_title = get_field('work_title', 'user_' . $author_id  . '');
+$work_phone = get_field('work_phone', 'user_' . $author_id  . '');
 $user_email = get_the_author_meta( 'email', $author_id );
 ?>
 
@@ -11,23 +12,25 @@ $user_email = get_the_author_meta( 'email', $author_id );
 
 		<div class="row" role="main">
 
-		    <div class="col s12">
+					<header class="col s12 center">
+						<h1 class="page-title light"><?php echo get_the_author_meta( 'display_name', $author_id );?> </h1>
 
-					<header>
-						<h1 class="page-title light center"><?php echo get_the_author_meta( 'display_name', $author_id );?> </h1>
 
-					</header>
 
 					<?php
 						if ($user_image) {
-							echo '<div class="col red s12"><img class="responsive-img circle col s2 offset-s5" src="' . $user_image . '" /></div>';
+							echo '<div class="col s12"><img class="responsive-img circle col s2 offset-s5" src="' . $user_image . '" /></div>';
 						}
 						if ($work_title){
-							echo '<div class="col s12 center">' . $work_title . '</div>';
+							echo '<div class="col s12">' . $work_title . '</div>';
 						}
-						echo '<div class="col s12 center"><a href="mailto:' . $user_email . '" target="_blank">' . $user_email . '</a></div>
-
-						<div class="col">' . get_the_author_meta( 'description', $author_id ) . '</div>';
+						echo '<div class="col s12"><a href="mailto:' . $user_email . '" target="_blank">' . $user_email . '</a></div>';
+						if ($work_phone){
+							echo '<div class="col s12">' . $work_phone . '</div>';
+						}?>
+						</header>
+					<?php
+						echo '<hr /><div class="col s12">' . get_the_author_meta( 'description', $author_id ) . '</div>';
 					?>
 
 					<h2 id="author-content" class="col s12 light center">Contributions</h2>
@@ -50,7 +53,7 @@ $user_email = get_the_author_meta( 'email', $author_id );
 
 					?>
 
-				</div>
+
 
 			<?php //get_sidebar('archives'); ?>
 

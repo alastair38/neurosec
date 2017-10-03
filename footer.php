9b39ab69525aa4518fd5fe5a63b88ee8af7112bf
alastair38 <alastair@alastaircox.com>
2017-10-03 13:34:22 +0100

    Version 1.43 - added twitter timeline option into footer

--- footer.php
+++ footer.php
@@ -1,3 +1,16 @@
+
+
+<?php if(is_front_page()){?>
+	<div  id="twitter_feed" class="col s12 white">
+		<div class="container">
+		<?php
+		$twitter_handle = get_theme_mod('tcx_twitter_handle');
+		echo do_shortcode( '[rotatingtweets screen_name="' . $twitter_handle . '" official_format="1" rotation_type="scrollVert" links_in_new_window="1" timeout="6000"]' );?>
+		</div>
+<?php }?>
+
+
+</div>
 		</main> <!-- end .wrapper -->
 
 		<footer id="contact" class="page-footer center">
